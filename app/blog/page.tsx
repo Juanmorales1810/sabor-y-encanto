59bd392b201fcda0c3ac64a7364b9b2503175b28
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-06-08 14:06:52 -0300

    Fix: Se corrigió las paginas de blog y menu

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,8 +2,9 @@ import { title } from "@/components/primitives";
 
 export default function BlogPage() {
 	return (
-		<div>
+		<div className="text-center">
 			<h1 className={title()}>Blog</h1>
+			<p className="text-2xl pt-4">&#128679;Esta pagina esta en proceso&#128679;</p>
 		</div>
 	);
 }
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,8 +2,9 @@ import { title } from "@/components/primitives";
 
 export default function PricingPage() {
 	return (
-		<div>
-			<h1 className={title()}>Pricing</h1>
+		<div className="text-center">
+			<h1 className={title()}>Menu</h1>
+			<p className="text-2xl pt-4">&#128679;Esta pagina esta en proceso&#128679;</p>
 		</div>
 	);
 }
