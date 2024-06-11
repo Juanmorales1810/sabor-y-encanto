d71f1c0bef8a0e87eddd8ddcbd84220952e3c760
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-06-11 14:20:26 -0300

    Feat: se agrego Blogpost

---
+++ config/blogPost.ts
@@ -0,0 +1,71 @@
+export const blogPosts = [
+    {
+        titulo: "Descubre la Auténtica Gastronomía Peruana en Sabor y Encanto, La Serena",
+        description:
+            "Bienvenidos al blog de Sabor y Encanto, donde celebramos la auténtica gastronomía peruana en el corazón de La Serena, Chile. Nuestro restaurante es más que un lugar para comer; es una experiencia culinaria que te transportará a las diversas regiones del Perú. En este blog, exploraremos algunos de los platos más emblemáticos que ofrecemos y el ambiente único que hemos creado para nuestros comensales.",
+        imageBlog: "/blogs/1.webp",
+        imageCard: "/blogs/2.webp",
+        slug: "descubre-la-autentica-gastronomia-peruana-en-sabor-y-encanto-la-serena",
+        content: [
+            {
+                title: "Ceviche Fresco: Delicia Peruana en Sabor y Encanto",
+                text: "El ceviche es uno de los platos más representativos de la cocina peruana. En Sabor y Encanto, nos enorgullecemos de preparar ceviches frescos utilizando pescado del día marinado en jugo de limón, cebolla morada, y cilantro, acompañado de camote y choclo. Este plato no solo es una explosión de sabores, sino también una muestra de la frescura y calidad de nuestros ingredientes. Si visitas nuestro restaurante, no puedes dejar de probar esta delicia.",
+            },
+            {
+                title: "Anticuchos: Tradición y Sabor en Cada Bocado",
+                text: "Los anticuchos son otra joya de la gastronomía peruana. En Sabor y Encanto, nuestros anticuchos suculentos están hechos con corazón de res marinado en una mezcla especial de especias y luego asados a la perfección. Servidos con papas doradas y ají, cada bocado es una combinación perfecta de sabores y texturas. Este plato es ideal para compartir y disfrutar con amigos y familiares en nuestro acogedor restaurante en La Serena.",
+            },
+            {
+                title: "Lomo Saltado: Un Clásico Peruano en Sabor y Encanto",
+                text: "El lomo saltado es un plato clásico que no puede faltar en nuestro menú. Este salteado de carne de res con cebolla, tomate y papas fritas, acompañado de arroz blanco, es un ejemplo perfecto de la fusión culinaria peruana. En Sabor y Encanto, nos aseguramos de que cada lomo saltado sea preparado con los ingredientes más frescos y las técnicas tradicionales, brindándote una experiencia auténtica en cada visita.",
+            },
+            {
+                title: "Postres Peruanos: Dulces Sabores en Sabor y Encanto",
+                text: "No puedes terminar tu comida sin probar nuestros postres peruanos. Desde el clásico suspiro a la limeña hasta la deliciosa mazamorra morada, cada postre en Sabor y Encanto está hecho con amor y respeto por las tradiciones peruanas. Estos dulces sabores son el cierre perfecto para una comida memorable.",
+            },
+            {
+                title: "Ambiente Acogedor y Elegante en Nuestro Restaurante en La Serena",
+                text: "En Sabor y Encanto, hemos creado un ambiente acogedor y elegante que complementa perfectamente nuestra oferta culinaria. Nuestro restaurante en La Serena está diseñado para que te sientas como en casa mientras disfrutas de la mejor gastronomía peruana. Nuestro equipo de servicio se esfuerza por ofrecerte una experiencia cálida y atenta, haciendo de cada visita algo inolvidable.",
+            },
+        ],
+        conclusion:
+            "Sabor y Encanto es el lugar ideal para descubrir y disfrutar de la auténtica gastronomía peruana en La Serena, Chile. Desde nuestros ceviches frescos y anticuchos suculentos hasta el clásico lomo saltado y nuestros dulces postres, cada plato es una celebración de los sabores únicos del Perú. Te invitamos a visitarnos y dejarte seducir por la experiencia culinaria que ofrecemos. ¡Te esperamos en Sabor y Encanto!",
+    },
+    {
+        titulo: "Variedad de Platos Peruanos en Sabor y Encanto, La Serena",
+        description:
+            "Bienvenidos a Sabor y Encanto, tu destino para la auténtica cocina peruana en La Serena, Chile. En este blog, vamos a sumergirnos en la rica variedad de platos que ofrecemos en nuestro restaurante. Cada plato está preparado con ingredientes frescos y técnicas tradicionales, asegurando que cada bocado sea una verdadera representación de la gastronomía peruana.",
+        imageBlog: "/blogs/3.webp",
+        imageCard: "/blogs/4.webp",
+        slug: "varidad-de-platos-peruanos-en-sabor-y-encanto-la-serena",
+        content: [
+            {
+                title: "Entradas Peruanas: Causas, Papas a la Huancaína y Más",
+                text: "Las entradas son la mejor manera de comenzar tu viaje culinario en Sabor y Encanto. La causa rellena, un plato hecho de puré de papas amarillas sazonadas con ají y limón, rellena de pollo o mariscos, es una de nuestras especialidades. Otra entrada imprescindible es la papa a la huancaína, papas hervidas cubiertas con una cremosa salsa de queso y ají amarillo. Estas entradas no solo son deliciosas, sino que también representan la diversidad y riqueza de la cocina peruana.",
+            },
+            {
+                title: "Especialidades Marinas: Arroz con Mariscos y Chupe de Camarones",
+                text: "Si eres amante de los mariscos, te encantará nuestra selección de especialidades marinas. El arroz con mariscos es un plato lleno de sabor, con arroz cocido en un caldo de mariscos y mezclado con una variedad de frutos del mar. El chupe de camarones, una sopa espesa hecha con camarones, papas, maíz y queso, es otro plato destacado que no puedes dejar de probar. Estos platos capturan la esencia de la costa peruana y son perfectos para disfrutar en cualquier ocasión.",
+            },
+            {
+                title: "Sabores de la Sierra Peruana: Ají de Gallina y Tacu Tacu",
+                text: "La sierra peruana también está representada en nuestro menú. El ají de gallina es un plato cremoso hecho con pechuga de pollo desmenuzada, ají amarillo, pan y leche, servido con arroz y papas. El tacu tacu, un plato de arroz y frijoles cocidos juntos hasta formar una masa, a menudo acompañado de lomo de res, es otra especialidad que refleja la riqueza culinaria de las montañas peruanas. Estos platos te transportarán a las alturas de los Andes con cada bocado.",
+            },
+            {
+                title: "Pisco Sour: El Cóctel Emblemático de Perú en Sabor y Encanto",
+                text: "Ninguna comida peruana está completa sin un pisco sour. Este cóctel emblemático de Perú, hecho con pisco, jugo de limón, jarabe de azúcar y clara de huevo, es el acompañamiento perfecto para cualquier plato en nuestro menú. En Sabor y Encanto, preparamos cada pisco sour con el equilibrio perfecto de ingredientes, asegurando una experiencia refrescante y deliciosa.",
+            },
+            {
+                title: "Nuestro Compromiso con la Autenticidad y Calidad",
+                text: "En Sabor y Encanto, estamos comprometidos con la autenticidad y la calidad. Desde la selección de ingredientes frescos hasta la preparación de cada plato, nos esforzamos por ofrecerte lo mejor de la gastronomía peruana. Nuestro chef, con amplia experiencia en cocina peruana, se asegura de que cada plato sea una obra maestra culinaria.",
+            },
+            {
+                title: "Ambiente y Servicio: Una Experiencia Completa en Sabor y Encanto",
+                text: "El ambiente en Sabor y Encanto es tan importante como la comida. Hemos creado un espacio acogedor y elegante donde puedes relajarte y disfrutar de una comida excepcional. Nuestro personal está dedicado a brindarte un servicio cálido y atento, asegurando que cada visita sea una experiencia memorable. Queremos que te sientas como en casa y disfrutes de cada momento en nuestro restaurante.",
+            },
+        ],
+        conclusion:
+            "La variedad de platos peruanos en Sabor y Encanto te ofrece una experiencia culinaria única en La Serena, Chile. Desde nuestras deliciosas entradas y especialidades marinas hasta los sabores auténticos de la sierra peruana y nuestro emblemático pisco sour, cada plato está diseñado para deleitar tus sentidos. Te invitamos a visitarnos y descubrir por qué Sabor y Encanto es el lugar perfecto para disfrutar de la auténtica gastronomía peruana. ¡Te esperamos con los brazos abiertos!",
+    },
+    // Agrega más entradas de blog relacionadas con la edición de video y cinematografía
+];
