36d32a94f7d941ed2bb8d53fc78b7f3204ef5ee4
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-06-11 14:22:35 -0300

    Fix: Se corrigió la UI de la pagina blog

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,8 +4,8 @@ export default function BlogLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<section className="flex flex-col items-center justify-start w-full h-[calc(100vh-64px)] gap-4">
-			<div className="flex flex-col items-center justify-center w-full h-full">
+		<section className="flex flex-col items-center justify-start w-full h-full min-h-[calc(100vh-64px)] gap-4">
+			<div className="flex flex-col items-center w-full h-full">
 				{children}
 			</div>
 		</section>
