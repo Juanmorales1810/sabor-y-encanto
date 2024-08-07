5177e7bbf0dc0884ebcc9a0935cfcfc7cdd25f86
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-08-07 20:43:12 -0300

    Fix: Se cambio el titulo y las palabras claves

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-    name: "Sabor y Encanto",
+    name: "Sabor y Encanto | Comida Peruana en La Serena, Chile",
     description:
         "Descubre Sabor y Encanto, un restaurante en La Serena, Chile, que celebra la rica y diversa gastronomía peruana. Ubicado en el corazón de la ciudad, nuestro espacio te invita a un viaje culinario a través del Perú, desde la costa hasta los Andes y la selva.",
     navItems: [
