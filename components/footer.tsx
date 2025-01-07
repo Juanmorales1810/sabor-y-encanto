044ddb1b69ca5711d0eabe640757c38079a0a705
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-01-07 12:33:24 -0300

    Fix: Se cambio el link de instagram

--- components/footer.tsx
+++ components/footer.tsx
@@ -30,7 +30,7 @@ export default function footer() {
                                     <Link target="_blank" href={`https://wa.me/+56987060388?text=Hola!%20Me%20gustaria%20hacer%20una%20reserva.%20Tenes%20alguna%20reservacion%20disponible?%20Muchas%20gracias!😊`} className="hover:underline">WhatsApp</Link>
                                 </li>
                                 <li className="mb-4">
-                                    <Link href="https://www.instagram.com/restaurant_sabor_y_encanto/profilecard/?igsh=MXFheWtraG9nZWw2cw==" className="hover:underline ">Instagram</Link>
+                                    <Link href="https://www.instagram.com/restaurant_sabor_y_encanto/" className="hover:underline ">Instagram</Link>
                                 </li>
                                 <li>
                                     <Link href="https://www.facebook.com/share/1B7kxBYXQj/?mibextid=wwXIfr" className="hover:underline">Facebook</Link>
@@ -61,7 +61,7 @@ export default function footer() {
                             </svg>
                             <span className="sr-only">Facebook page</span>
                         </Link>
-                        <Link href="https://www.instagram.com/restaurant_sabor_y_encanto/profilecard/?igsh=MXFheWtraG9nZWw2cw==" className="text-secondary-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                        <Link href="https://www.instagram.com/restaurant_sabor_y_encanto/" className="text-secondary-500 hover:text-gray-900 dark:hover:text-white ms-5">
                             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
                                 <path fillRule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clipRule="evenodd" />
                             </svg>
